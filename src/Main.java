16ea9151a5b061d3987857839049caa02ee36208
tomoyasu <tomoyasu0504.9115@gmail.com>
2023-06-13 15:18:38 +0900

    first commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Human> HumanList = new ArrayList<>();
+        HumanList.add(new Human("Ichiro", 31));
+        HumanList.add(new Human("Ziro", 22));
+        HumanList.add(new Human("Saburo",23 ));
+
+        for(Human human : HumanList) {
+            if (human.getName().equals("Ichiro")){
+                System.out.println("名前" + human.getName());
+                System.out.println("年齢" + human.getAge());
+            }
+        }
+
+    }
+}
