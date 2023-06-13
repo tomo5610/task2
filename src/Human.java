e49416ee4471166e88319e519ead5b27775fbf34
tomoyasu <tomoyasu0504.9115@gmail.com>
2023-06-13 17:32:31 +0900

    secomd commit

--- src/Human.java
+++ src/Human.java
@@ -1,22 +1,23 @@
 public class Human {
 
-        private  String name;
+    private String name;
 
-        private int age;
+    private int age;
 
-        public Human(String name, int age) {
-            this.name = name;
-            this.age = age;
-        }
+    public Human(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
 
-        public String getName() {
-            return name;
-        }
-        public int getAge() {
-            return age;
-        }
+    public String getName() {
+        return name;
+    }
 
-        public void setName(String name) {
-            this.name = name;
-        }
+    public int getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,11 @@ public class Main {
     public static void main(String[] args) {
         List<Human> HumanList = new ArrayList<>();
         HumanList.add(new Human("Ichiro", 31));
-        HumanList.add(new Human("Ziro", 22));
-        HumanList.add(new Human("Saburo",23 ));
+        HumanList.add(new Human("Jiro", 22));
+        HumanList.add(new Human("Saburo", 23));
 
-        for(Human human : HumanList) {
-            if (human.getName().equals("Ichiro")){
+        for (Human human : HumanList) {
+            if (human.getName().equals("Ichiro")) {
                 System.out.println("名前" + human.getName());
                 System.out.println("年齢" + human.getAge());
             }
